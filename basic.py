211a1be937352f7dbf62493c8d1f3885f10868fe
77035338 <MY_NAME@example.com>
2021-11-13 11:59:31 -0500

    welp

---
+++ basic.py
@@ -0,0 +1,7 @@
+numDays = int(input("Enter the number of days: "));
+#print(numDays, "days");
+days_to_sec = numDays*60*60*24;
+if numDays==1:
+  print(numDays,"day is equal to",days_to_sec,"seconds");
+else:
+  print(numDays,"days is equal to",days_to_sec,"seconds");
---
+++ main.py
@@ -0,0 +1,42 @@
+message = "banana";
+fstring = "jeff's melody";
+fstring_ex2 = "mia's melody";
+cars = ["honda","mclaren","Mercedes-Benz"];
+print(message);
+print(cars[0:2]);
+print(cars[-0:-2]);
+print(f"I love {fstring}, it's way better than {fstring_ex2}!")
+for yeet in "m u lti_l ine_ex_l e t ter":
+  print(yeet);
+
+melon = 0;
+while melon < 5:
+ print('melon!');
+
+ melon += 1 
+ continue
+
+
+calculationTo_SecondsEasier = 24*60*60;
+name_of_timeUnit = "seconds";
+
+def noRewrite_forCalc_daysToSec(num_ofDays):
+  print(f"{num_ofDays} days are {num_ofDays * calculationTo_SecondsEasier} {name_of_timeUnit}")
+  global grmmerfixfr1;
+
+  if num_ofDays < 2 and num_ofDays > 0 and num_ofDays > -2:
+    grmmerfixfr1 = "day is";
+  else:
+    grmmerfixfr1 = "days are";
+  
+
+#               day|hrs|min|sec 
+print(f" day is {1*24*60*60} seconds");
+#     OR    | no. of days | # of days to sec  |  unit name (in this case = sec)
+print(f"1 {grmmerfixfr1} {1*calculationTo_SecondsEasier} {name_of_timeUnit}");
+print(f"2 {grmmerfixfr1} {2*calculationTo_SecondsEasier} {name_of_timeUnit}");
+print(f"3 {grmmerfixfr1} {3*calculationTo_SecondsEasier} {name_of_timeUnit}");
+
+#how to call/ activate the funtion above, (similar to JavaScript)
+noRewrite_forCalc_daysToSec(2)
+        #the vallue/number above is the number of days
